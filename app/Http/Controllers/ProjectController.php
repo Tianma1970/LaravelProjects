5589f6ffebd53d43b93033bbda20e9f35d40d503
Tillmann Weimer <tillmann1970@gmail.com>
2020-01-09 16:14:38 +0100

    added methods

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Project;
 use Illuminate\Http\Request;
-
 class ProjectController extends Controller
 {
-    //
+
+    /**
+     * Show an index of all
+     */
+    public function index(){
+        $projects = Project::all();
+
+        return view('projects/index', [
+            'projects'  => $projects
+            ]);
+        }
+
+        public function show($id){
+            $project = Project::findOrFail($id);
+
+            return view('projects/show', [
+                'project'   => $project
+            ]);
+        }
 }
