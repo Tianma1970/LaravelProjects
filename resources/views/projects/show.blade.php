1d6162ea6eebdb96b371bf48bf9dc48e24ee8c91
Tillmann Weimer <tillmann1970@gmail.com>
2020-01-12 22:22:18 +0100

    added link and submit button

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -11,8 +11,19 @@
             <li>{{ $todo->title }}</li>
         @endforeach
     </ol>
-
-    <a href="/projects">&laquo; Tillbaka till alla projekt</a>
+    <div>
+        <a href="/projects">&laquo; Back to all Projects</a>
+    </div>
+    <div>
+        <a href="/projects/{{ $project->id }}/edit" class="btn btn-warning mt-3">Edit Project</a>
+        <form method="POST" action="/projects/{{ $project->id }}">
+            @csrf
+            @method("DELETE")
+            <input type="submit" value="Delete Project" class="btn btn-danger mt-3">
+        </form>
+    </div>
+    <div>
+    </div>
 
 </div>
 
