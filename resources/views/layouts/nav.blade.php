a81c0006e0e0f1d072ff00ee5ffd22f46161fcba
Tillmann Weimer <tillmann1970@gmail.com>
2020-01-13 17:30:00 +0100

    added link

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -11,6 +11,9 @@
 	    		<li class="nav-item">
 	    			<a class="nav-link" href="/">Welcome</a>
 	    		</li>
+	    		<li class="nav-item">
+	    			<a class="nav-link" href="/projects">Projects</a>
+	    		</li>
 
 	    	</ul>
         </div>
