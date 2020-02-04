0dfacc67be5f8988e87a1d62ac88dccb85a1cdad
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-04 15:02:52 +0100

    added bootstrap class

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,11 @@
     <div class="container mt-4">
 
         <h1 class="text-center"><i>Welcome</i></h1>
-        <div class="jumbotron col-4 text-center mt-5">
-            <p>{{ $msg }}<br>
-            <p><small>{{ $info }}</small></p>
+        <div class="row justify-content-md-center">
+            <div class="jumbotron col-lg-4 text-center mt-5">
+                <p>{{ $msg }}<br>
+                <p><small>{{ $info }}</small></p>
+            </div>
         </div>
     </div><!--/container-->
 
