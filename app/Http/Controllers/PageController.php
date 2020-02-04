ef35be6bcc3231d2a7d58ba5728781b180649192
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-04 13:29:06 +0100

    added variable $info

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,7 +14,10 @@ class PageController extends Controller
         if(Auth::check()) {
             $user = Auth::user();
             $msg = "You are logged in as {$user->name}";
+            $info = "You created your account at {$user->created_at}";
         }
-        return view('welcome', ['msg' => $msg]);
+        return view('welcome', ['msg' => $msg,
+                                'info' => $info
+                                ]);
     }
 }
