2c4f6d1c2df2815dd6c069beddbbf7adb900dab0
Tillmann Weimer <tillmann1970@gmail.com>
2020-02-05 13:18:11 +0100

    moved folder

---
+++ resources/views/projects/todos/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container mt-3">
+
+    <h1>Create a new Task </h1>
+
+    <form method="POST" action="/projects/{{ $project->id }}/todos">
+
+        @csrf
+
+        <!--vi måste sätte en token av säkerhetsskäl annars kan formuläret inte skickas-->
+        @csrf <!--står för cross-site rquest forgery-->
+        <div class="form-group">
+            <label for="title">Todo Title</label>
+            <input type="text" name="title" class="form-control" placeholder="Todo Title">
+        </div>
+        <div class="form-group">
+            <label for="description">Todo Description</label>
+            <input type="text" name="description" class="form-control" placeholder="Todo Description">
+        </div>
+        <div class="mt-3">
+            <input  type="submit" value="Create New Todo" class="btn btn-primary">
+        </form>
+        </div>
+
+    <a href="/projects/{{ $project->id }}">&laquo; Back to all todos</a>
+
+</div>
+
+@endsection
